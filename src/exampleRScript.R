c3ac96a6cb7cb412de0bb632c99390ec33a85e5c
olin-feist <55856303+olin-feist@users.noreply.github.com>
2020-07-12 00:35:02 -0700

    Update exampleRScript.R

--- src/exampleRScript.R
+++ src/exampleRScript.R
@@ -4,4 +4,4 @@ sourceCpp("parseCsv.cpp")
 
 #call c++ method to parse CSV file
 #parsefile([filepath],[interval],[variance])
-parsefile("C:/Users/olin/Desktop/AIS_ASCII_by_UTM_Month/2017_v2/AIS_2017_01_Zone10.csv",10,5)
+parsefile("/.../AIS_ASCII_by_UTM_Month/2017_v2/AIS_2017_01_Zone10.csv",10,5)
